8fbed86b08363e5b8df1a703831b51edca80d060
Nigel Bunner <nigelbunner@gmail.com>
2019-03-05 17:29:55 +0000

    styling navbar on mobile

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -60,7 +60,7 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:image" content="/img/og-image.jpg" />
         </Helmet>
         <Navbar />
-        <div>{children}</div>
+        <div class="main-body">{children}</div>
         <Footer />
       </div>
     )}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,10 +19,12 @@ const Navbar = class extends React.Component {
           // Get the target from the "data-target" attribute
           const target = el.dataset.target;
           const $target = document.getElementById(target);
+          const $documentEl = document.querySelector("body");
 
           // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
           el.classList.toggle("is-active");
           $target.classList.toggle("is-active");
+          $documentEl.classList.toggle("mobNavOpen");
         });
       });
     }
