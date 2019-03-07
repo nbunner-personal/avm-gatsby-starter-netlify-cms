7b2a08a507cbec278f541c3c330b4191f05edbd2
Nigel Bunner <nigelbunner@gmail.com>
2019-03-07 12:59:21 +0000

    capital letter typo preventing build

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 // import { Link } from 'gatsby'
-import heroStyles from './heroImage.module.css'
+import heroStyles from './heroimage.module.css'
 
 
 const hero = ({ heroImage, heroMsg }) => (
