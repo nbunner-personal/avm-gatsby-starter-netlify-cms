734b58df339a69e73fdf0c22ad7d981959fb1665
Nigel Bunner <nigelbunner@gmail.com>
2019-03-08 21:49:33 +0000

    Adding blog posts up to august 2018 & events index page

---
+++ src/components/EventsRoll.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import postStyles from './posts.module.css'
+
+class EventsRoll extends React.Component {
+
+    render() {
+        const { data } = this.props
+        const { edges: posts } = data.allMarkdownRemark
+
+        return (
+            <section className={postStyles.cont}>
+                {posts && (posts
+                    .map(({ node: post }) => (
+                        <div
+                            className={postStyles.card}
+                            key={post.id}
+                        >
+                            <article className={postStyles.cardContent}>
+                                <p>
+                                    <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                                        {post.frontmatter.title}
+                                    </Link>
+                                    <span> &bull; </span>
+                                    <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
+                                </p>
+                                <p>
+                                    {post.excerpt}
+                                    <br />
+                                    <br />
+                                    <Link className="button" to={post.fields.slug}>
+                                        Keep Reading →
+                </Link>
+                                </p>
+                            </article>
+                        </div>
+                    )))}
+            </section>
+        );
+    }
+}
+
+EventsRoll.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array,
+        }),
+    }),
+}
+
+export default () => (
+    <StaticQuery
+        query={graphql`
+    query EventsRollQuery {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] },
+        filter: { frontmatter: { templateKey: { eq: "events-post" } }}
+      ) {
+        edges {
+          node {
+            excerpt(pruneLength: 400)
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              templateKey
+              date(formatString: "MMMM DD, YYYY")
+            }
+          }
+        }
+      }
+    }
+    `}
+        render={(data, count) => (
+            <EventsRoll data={data} count={count} />
+        )}
+    />
+)
---
+++ src/pages/news/2007-09-12-august-07-report.md
@@ -0,0 +1,14 @@
+---
+layout: post
+templateKey: blog-post
+title: "August '07 Report"
+published: true
+author: Pip
+comments: false
+date: 2007-09-12 11:09:09
+tags: [ ]
+categories:
+    - reports
+permalink: /2007/09/12/august-07-report
+---
+Click here to see August &#8217;07 Report (PDF)
