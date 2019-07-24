7b6c06569db18bc9b6121cb85188add1b3965372
Nigel Bunner <nigelbunner@gmail.com>
2019-07-24 22:05:33 +0100

    Adding shop product page to the cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,28 +1,38 @@
-import CMS from 'netlify-cms'
+import CMS from "netlify-cms";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import AboutTeamPreview from "./preview-templates/AboutTeamPreview";
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import EventsPostPreview from './preview-templates/EventsPostPreview'
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import EventsPostPreview from "./preview-templates/EventsPostPreview";
 // import ProductPagePreview from './preview-templates/ProductPagePreview'
-import ProjectPagePreview from './preview-templates/ProjectPagePreview'
-import FeaturedProjectPagePreview from './preview-templates/FeaturedProjectPagePreview'
-import FeaturedProjectPageSimplePreview from './preview-templates/FeaturedProjectPageSimplePreview'
-import FeaturedProjectPageSimpleGalPreview from './preview-templates/FeaturedProjectPageSimpleGalPreview'
-import FeaturedProjectPageSimpleVidPreview from './preview-templates/FeaturedProjectPageSimpleVidPreview'
+import ProjectPagePreview from "./preview-templates/ProjectPagePreview";
+import FeaturedProjectPagePreview from "./preview-templates/FeaturedProjectPagePreview";
+import FeaturedProjectPageSimplePreview from "./preview-templates/FeaturedProjectPageSimplePreview";
+import FeaturedProjectPageSimpleGalPreview from "./preview-templates/FeaturedProjectPageSimpleGalPreview";
+import FeaturedProjectPageSimpleVidPreview from "./preview-templates/FeaturedProjectPageSimpleVidPreview";
 import PageBasicPreview from "./preview-templates/PageBasicPreview";
 import PageTextLinksPreview from "./preview-templates/PageTextLinksPreview";
+import ShopProductPagePreview from "./preview-templates/ShopProductPagePreview";
 
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("aboutTeam", AboutTeamPreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("events", EventsPostPreview);
 // CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('projects', ProjectPagePreview)
-CMS.registerPreviewTemplate('featuredProject', FeaturedProjectPagePreview)
-CMS.registerPreviewTemplate('featuredProjectSimple', FeaturedProjectPageSimplePreview)
-CMS.registerPreviewTemplate('featuredProjectSimpleGal', FeaturedProjectPageSimpleGalPreview)
-CMS.registerPreviewTemplate('featuredProjectSimpleVid', FeaturedProjectPageSimpleVidPreview)
-CMS.registerPreviewTemplate('pageTextLinks', PageTextLinksPreview);
+CMS.registerPreviewTemplate("projects", ProjectPagePreview);
+CMS.registerPreviewTemplate("featuredProject", FeaturedProjectPagePreview);
+CMS.registerPreviewTemplate(
+  "featuredProjectSimple",
+  FeaturedProjectPageSimplePreview
+);
+CMS.registerPreviewTemplate(
+  "featuredProjectSimpleGal",
+  FeaturedProjectPageSimpleGalPreview
+);
+CMS.registerPreviewTemplate(
+  "featuredProjectSimpleVid",
+  FeaturedProjectPageSimpleVidPreview
+);
+CMS.registerPreviewTemplate("pageTextLinks", PageTextLinksPreview);
 CMS.registerPreviewTemplate("pageBasic", PageBasicPreview);
-
+CMS.registerPreviewTemplate("shopProductPage", ShopProductPagePreview);
---
+++ src/cms/preview-templates/ShopProductPagePreview.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ShopProductPage } from "../../templates/shop-product-page";
+
+const ShopProductPagePreview = ({ entry, widgetFor }) => (
+  <ShopProductPageTemplate
+    title={entry.getIn(["data", "title"])}
+    content={widgetFor("body")}
+  />
+);
+
+ShopProductPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  widgetFor: PropTypes.func
+};
+
+export default ShopProductPagePreview;
