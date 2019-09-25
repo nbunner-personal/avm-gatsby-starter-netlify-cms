ffd700d06bf9601d8d2d5a5a4fd0c9b50cf81907
Nigel Bunner <nigelbunner@gmail.com>
2019-09-25 17:30:38 +0100

    Team page styling

--- src/templates/about-team.js
+++ src/templates/about-team.js
@@ -3,80 +3,83 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
-import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
+import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import FeaturedProjects from "../components/FeaturedProjects";
 import CtaButton from "../components/CtaButton";
-import teamStyles from '../components/team.module.css';
+import teamStyles from "../components/team.module.css";
 
-
-export const TeamTemplate = ({    
-    content,
-    malawiTeam,
-    ukTeam,
-    malawiTitle,
-    malawiText,
-    ukTitle,
-    ukText,
-    contentComponent
+export const TeamTemplate = ({
+  content,
+  malawiTeam,
+  ukTeam,
+  malawiTitle,
+  malawiText,
+  ukTitle,
+  ukText,
+  contentComponent
 }) => {
-    const PageContent = contentComponent || Content;
+  const PageContent = contentComponent || Content;
 
-    return (
-      <section className="section section--gradient">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-14 is-offset-1">
-              <article className="content">
-                <div className="columns">
-                  <main className="column is-8">
-                    <h1 className="has-text-weight-semibold is-size-2">
-                      Meet the team
-                    </h1>
-                    <h2>Team in Malawi</h2>
-                    <ul className={teamStyles.teamList}>
-                      {malawiTeam.map(team => (
-                        <li key={team.name}>
-                            <div className={teamStyles.teamPhoto}>
-                                <PreviewCompatibleImage imageInfo={team.photo} />
-                            </div>
-                          <h4>{team.name}</h4>
-                          <p>{team.role}</p>
-                        </li>
-                      ))}
-                    </ul>
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-14 is-offset-1">
+            <article className="content">
+              <div className="columns">
+                <main className="column is-8">
+                  <h1 className="has-text-weight-semibold is-size-2">
+                    Meet the team
+                  </h1>
+                  <h2>Team in Malawi</h2>
+                  <ul className={teamStyles.teamList}>
+                    {malawiTeam.map(team => (
+                      <li key={team.name}>
+                        <div className={teamStyles.teamPhoto}>
+                          <PreviewCompatibleImage imageInfo={team.photo} />
+                        </div>
+                        <h4>{team.name}</h4>
+                        <p>{team.role}</p>
+                      </li>
+                    ))}
+                  </ul>
+                  <div className={teamStyles.contentBox}>
                     <h2>{malawiTitle}</h2>
                     <p>{malawiText}</p>
                     <PageContent className="content" content={content} />
+                  </div>
+                  <div className={teamStyles.contentBox}>
                     <h2>{ukTitle}</h2>
                     <p>{ukText}</p>
-                    <h2>Team in the UK</h2>
-                    <ul className={teamStyles.teamList}>
-                      {ukTeam.map(team => (
-                          <li key={team.name}>
-                              <div className={teamStyles.teamPhoto}>
-                                  <PreviewCompatibleImage imageInfo={team.photo} />
-                              </div>
-                              <h4>{team.name}</h4>
-                              <p>{team.role}</p>
-                          </li>
-                      ))}
-                    </ul>
+                  </div>
+                  <h2>Team in the UK</h2>
+                  <ul className={teamStyles.teamList}>
+                    {ukTeam.map(team => (
+                      <li key={team.name}>
+                        <div className={teamStyles.teamPhoto}>
+                          <PreviewCompatibleImage imageInfo={team.photo} />
+                        </div>
+                        <h4>{team.name}</h4>
+                        <p>{team.role}</p>
+                      </li>
+                    ))}
+                  </ul>
 
-                    <CtaButton
-                      link="https://www.charitycheckout.co.uk/1113786/"
-                      text="Donate"
-                    />
-                  </main>
-                  <aside className="column is-4">
-                    <FeaturedProjects currentProject="default" />
-                  </aside>
-                </div>
-              </article>
-            </div>
+                  <CtaButton
+                    link="https://www.charitycheckout.co.uk/1113786/"
+                    text="Donate"
+                  />
+                </main>
+                <aside className="column is-4">
+                  <FeaturedProjects currentProject="default" />
+                </aside>
+              </div>
+            </article>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 TeamTemplate.propTypes = {
@@ -91,68 +94,68 @@ TeamTemplate.propTypes = {
 };
 
 const TeamPage = ({ data }) => {
-    const { markdownRemark: post } = data;
+  const { markdownRemark: post } = data;
 
-    return (
-      <Layout>
-        <TeamTemplate
-          contentComponent={HTMLContent}
-          title={post.frontmatter.title}
-          content={post.html}
-          malawiTeam={post.frontmatter.malawiTeam}
-          ukTeam={post.frontmatter.ukTeam}
-          malawiTitle={post.frontmatter.malawiTitle}
-          malawiText={post.frontmatter.malawiText}
-          ukTitle={post.frontmatter.ukTitle}
-          ukText={post.frontmatter.ukText}
-        />
-      </Layout>
-    );
+  return (
+    <Layout>
+      <TeamTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+        malawiTeam={post.frontmatter.malawiTeam}
+        ukTeam={post.frontmatter.ukTeam}
+        malawiTitle={post.frontmatter.malawiTitle}
+        malawiText={post.frontmatter.malawiText}
+        ukTitle={post.frontmatter.ukTitle}
+        ukText={post.frontmatter.ukText}
+      />
+    </Layout>
+  );
 };
 
 TeamPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object
-        })
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object
     })
+  })
 };
 
 export default TeamPage;
 
 export const TeamPageQuery = graphql`
-         query TeamPage($id: String!) {
-           markdownRemark(id: { eq: $id }) {
-             html
-             frontmatter {
-               title
-               malawiTitle
-               malawiText
-               ukTitle
-               ukText               
-               malawiTeam {
-                 photo {
-                   childImageSharp {
-                     fluid(maxWidth: 150, quality: 50) {
-                       ...GatsbyImageSharpFluid
-                     }
-                   }
-                 }
-                 name
-                 role
-               }
-               ukTeam {
-                 photo {
-                   childImageSharp {
-                     fluid(maxWidth: 150, quality: 50) {
-                       ...GatsbyImageSharpFluid
-                     }
-                   }
-                 }
-                 name
-                 role
-               }
-             }
-           }
-         }
-       `;
+  query TeamPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        malawiTitle
+        malawiText
+        ukTitle
+        ukText
+        malawiTeam {
+          photo {
+            childImageSharp {
+              fluid(maxWidth: 150, quality: 50) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          name
+          role
+        }
+        ukTeam {
+          photo {
+            childImageSharp {
+              fluid(maxWidth: 150, quality: 50) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          name
+          role
+        }
+      }
+    }
+  }
+`;
