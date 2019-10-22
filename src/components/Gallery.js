a4ae4f01c519fec6783a66bccd0e2e7c633ba9e9
Nigel Bunner <nigelbunner@gmail.com>
2019-10-22 21:49:03 +0100

    Adds close icon to lightbox

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,42 +1,41 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { v4 } from "uuid";
-import { Dialog } from "@reach/dialog"
-import '@reach/dialog/styles.css';
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
 import galleryStyles from "./gallery.module.css";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 import Img from "gatsby-image";
 // import { renderComponent } from "recompose";
 
 class Gallery extends Component {
-
   state = {
     showLightbox: false,
-    selectedImage: null,
-  }
+    selectedImage: null
+  };
 
-  showLightboxHandler = (event) => {
-    console.log('show lightbox');
+  showLightboxHandler = event => {
+    console.log("show lightbox");
     this.setState({
       showLightbox: true
-    })
-  }
+    });
+  };
 
-  hideLightboxHandler = (event) => {
-    console.log('hide lightbox');
+  hideLightboxHandler = event => {
+    console.log("hide lightbox");
     this.setState({
       showLightbox: false
-    })
-  }
+    });
+  };
 
   render() {
-    const { gallery } = this.props
-    
+    const { gallery } = this.props;
+
     return (
       <div className={galleryStyles.gallery}>
         {gallery.map(gall => (
           <span
-          className={galleryStyles.gridCell}
+            className={galleryStyles.gridCell}
             onClick={() =>
               this.setState({
                 showLightbox: true,
@@ -52,36 +51,40 @@ class Gallery extends Component {
         ))}
         {this.state.showLightbox && (
           <Dialog>
-            {/* img is {this.state.selectedImage} */}
-            <Img
-              fluid={this.state.selectedImage}
-            />
-            <button type="button" onClick={this.hideLightboxHandler}>
-              Close
-            </button>
+            <div className={galleryStyles.dialogInner}>
+              {/* img is {this.state.selectedImage} */}
+              <Img fluid={this.state.selectedImage} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                onClick={this.hideLightboxHandler}
+                className={galleryStyles.closeicon}
+              >
+                <path
+                  fill="#1a222d"
+                  d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
+                />
+              </svg>
+            </div>
           </Dialog>
         )}
       </div>
     );
   }
-
-
 }
-
-
 
 // export default Gallery;
 
 // const Gallery = ({ gallery, state, setstate }) => (
 //   <div className={galleryStyles.gallery}>
-//     {gallery.map(gall => (     
-//         <a 
+//     {gallery.map(gall => (
+//         <a
 //         onClick={this.showLightboxHandler}
 //         href={gall.photo.childImageSharp.fluid.src} key={v4()}>
 //           <figure className="image">
 //             <PreviewCompatibleImage imageInfo={gall.photo} />
 //           </figure>
-//         </a>          
+//         </a>
 //     ))}
 //   </div>
 // );
--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,40 +1,41 @@
-import React, { Component, Fragment } from "react"
-import PropTypes from "prop-types"
-import Img from "gatsby-image"
-import styled from "styled-components"
-import { Dialog } from "@reach/dialog"
-import "@reach/dialog/styles.css"
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
+import lightboxStyles from "../components/lightbox.module.css";
 
 const LightboxContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 5px;
-`
+`;
 
 const PreviewButton = styled.button`
   background: transparent;
   border: none;
   padding: 0;
   margin: 0;
-`
+`;
 
 export default class Lightbox extends Component {
   static propTypes = {
-    galleryImages: PropTypes.array.isRequired, // eslint-disable-line
-  }
+    galleryImages: PropTypes.array.isRequired // eslint-disable-line
+  };
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       showLightbox: false,
-      selectedImage: null,
-    }
+      selectedImage: null
+    };
   }
 
   render() {
-    const { galleryImages } = this.props
-    const { selectedImage, showLightbox } = this.state
+    const { galleryImages } = this.props;
+    const { selectedImage, showLightbox } = this.state;
     return (
       <Fragment>
         <LightboxContainer>
@@ -52,16 +53,23 @@ export default class Lightbox extends Component {
         </LightboxContainer>
         {showLightbox && (
           <Dialog>
-            <Img fluid={selectedImage.node.childImageSharp.fluid.src} />
-            <button
-              type="button"
-              onClick={() => this.setState({ showLightbox: false })}
-            >
-              Close
-            </button>
+            <div className={lightboxStyles.dialogInner}>
+              <Img fluid={selectedImage.node.childImageSharp.fluid.src} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                onClick={() => this.setState({ showLightbox: false })}
+                className={lightboxStyles.closeicon}
+              >
+                <path
+                  fill="#58b5d7"
+                  d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"
+                />
+              </svg>
+            </div>
           </Dialog>
         )}
       </Fragment>
-    )
+    );
   }
 }
