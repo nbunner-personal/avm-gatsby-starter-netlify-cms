2cd2c27d75215ae2661a1d60b71aedadd31dffa4
Nigel Bunner <nigelbunner@gmail.com>
2019-10-28 14:08:52 +0000

    Admin updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: "African Vision Malawi",
     description:
-      "Since 2005 African Vision Malawi has been helping children and vulnerable people in Malawi, one of the poorest countries in the world. Our vision is to see a healthy, educated and self-sufficient community in Malawi."
+      "Since 2005 African Vision Malawi has been helping children and vulnerable people in Malawi, one of the poorest countries in the world. Our vision is to see a healthy, educated and self-sufficient community in Malawi.."
   },
   plugins: [
     "gatsby-plugin-react-helmet",
